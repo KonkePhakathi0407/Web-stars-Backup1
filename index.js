89615c8c6d47a512062d144efb7ac951d2433e70
Zamaluthuli28 <zamanonophaluthuli@gmail.com>
2026-03-10 15:37:35 +0200

    Initial Push

---
+++ index.js
@@ -0,0 +1,7 @@
+burger = document.querySelector(".burger");
+navbarItems = document.querySelector(".navbar-items ul");
+nav = document.querySelector(".nav");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
